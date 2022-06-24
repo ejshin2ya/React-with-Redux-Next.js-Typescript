dc6ccd6c0564e06c8ba0ef809669919ae2ad9214
신은정 <ejshin0213@naver.com>
2022-06-24 23:59:04 +0900

    ErrorModal 컴포넌트 추가하기

--- React/8.Practice/src/components/Users/AddUser.js
+++ React/8.Practice/src/components/Users/AddUser.js
@@ -8,13 +8,27 @@ const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
 
+  const [error, setError] = useState();
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age(non-empty values).",
+      });
       return;
     }
     if (+enteredAge < 1) {
       //문자열로 되어있기 때문에 +를 붙여주면 숫자로 변환
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age(>0).",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -32,7 +46,13 @@ const AddUser = (props) => {
 
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
