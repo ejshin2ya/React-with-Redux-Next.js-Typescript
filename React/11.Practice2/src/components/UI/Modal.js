5f7570289b93d371a6f420d6e5f62f6a7f1a0685
신은정 <ejshin0213@naver.com>
2022-07-02 03:03:10 +0900

    카트 및 모달 State 관리

--- React/11.Practice2/src/components/UI/Modal.js
+++ React/11.Practice2/src/components/UI/Modal.js
@@ -1,8 +1,9 @@
 import classes from "./Modal.module.css";
 import { Fragment } from "react";
 import ReactDOM from "react-dom";
+
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -18,7 +19,10 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
