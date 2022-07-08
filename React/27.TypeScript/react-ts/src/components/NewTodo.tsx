5d626015169bab338964d6a115e7d02b99b9ef8c
신은정 <ejshin0213@naver.com>
2022-07-08 19:04:48 +0900

    컨텍스트 API 및 TypeScript

--- React/27.TypeScript/react-ts/src/components/NewTodo.tsx
+++ React/27.TypeScript/react-ts/src/components/NewTodo.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useRef } from "react";
 import classes from "./NewTodo.module.css";
+import { TodosContext } from "../store/todos-context";
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -14,8 +16,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
       //throw an error
       return;
     }
-
-    props.onAddTodo(enteredText);
+    todosCtx.addTodo(enteredText);
   };
 
   return (
