41aa43b1eb5ca45034b14a8d8f2269355458a95b
신은정 <ejshin0213@naver.com>
2022-07-10 00:16:47 +0900

    사용자에게 피드백 추가하기

--- React/22.JWT/src/components/Auth/AuthForm.js
+++ React/22.JWT/src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -18,7 +19,7 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     //optional:Add validation
-
+    setIsLoading(true);
     if (isLogin) {
     } else {
       fetch(
@@ -35,12 +36,16 @@ const AuthForm = () => {
           },
         }
       ).then((res) => {
+        setIsLoading(false);
         if (res.ok) {
           //...
         } else {
           return res.json().then((data) => {
-            //show an error modal
-            console.log(data);
+            let errorMessage = "Authentication failed!!";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage);
           });
         }
       });
@@ -65,7 +70,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
